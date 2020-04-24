fc2aea5133380398e4ce7e7054a4b934d814f3ad
pranaykgupta <pranaykgupta21@gmail.com>
2020-04-24 23:17:50 +0530

    able to show tasks from the blockchain

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public taskcount = 0;
+    uint public taskCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Started with the ethereum app");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
